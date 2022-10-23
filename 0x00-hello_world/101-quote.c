efb02376c225f884c9500732d01df03d4fc416ac
chimekinglsey <onlinekingsley@gmail.com>
2022-10-22 18:05:16 -0700

    boom

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Printing to standaard error
+ * Return: always return 0;
+ **/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print sizes of variable based on processor type
+ * Return: always return 0
+ **/
+int main(void)
+{
+	char = 'A';
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
+	printf("Size of a char: %d\n", sizeof('A'));
+	printf("Size of an int: %d\n", sizeof(i));
+	printf("Size of a long int: %d\n", sizeof(li));
+	printf("Size of a long long int: %d\n", sizeof(lli));
+	printf("Size of a float: %d\n", sizeof(f));
+	return (0);
+}
