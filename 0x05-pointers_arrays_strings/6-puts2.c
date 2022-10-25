937ce69b2d76c2330c66c9f54e750c895fd5ae51
chimekinglsey <onlinekingsley@gmail.com>
2022-10-25 16:47:57 -0700

    mustwork

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,17 +11,19 @@ int i;
 
 for (i = 0; str[i] != '\0'; i++)
 {
-	if ((i % 2) == 2)
+	if (i % 2 == 2)
 	{
 	_putchar(str[i]);
 	}
-	else if ((i % 2) != 1)
+	else if (i % 2 != 1)
 	{
 		_putchar(str[i]);
 	}
 	else
 	{
-		_putchar(str[i]);
+	/*
+	 * _putchar(str[i]);
+	 */
 	}
 }
 }
