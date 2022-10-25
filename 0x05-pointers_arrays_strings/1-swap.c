0e71065e43e1ca546db80dfad023572a4efacb06
chimekinglsey <onlinekingsley@gmail.com>
2022-10-24 21:10:59 -0700

    seconded

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "main.h"
-/** swap_int - swaps the values of integer '*a' and '*b'
- * 
+/**
+ *swap_int - swaps the values of integer '*a' and '*b'
  *@a: takes the values of int and swaps
+ *@b: takes the seond integer value and swaps it
  * Return: nothing
  **/
 void swap_int(int *a, int *b)
@@ -9,4 +10,4 @@ void swap_int(int *a, int *b)
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
-} 
+}
