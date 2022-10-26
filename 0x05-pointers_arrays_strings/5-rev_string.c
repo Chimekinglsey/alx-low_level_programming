468670a33e624c470ddbb0253a975c796d28e956
chimekinglsey <onlinekingsley@gmail.com>
2022-10-25 17:18:00 -0700

    divs

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,37 +1,18 @@
 #include "main.h"
 /**
- * _strlen - returns the length of a given string
- *
- * @s: string whose length is to be evaluated
- * Return: returns the length of string
- */
-int _strlen(char *5)
-{
-int i;
-
-for (i = 0; s[i] != '\0'; i++)
-{
-return (i);
-}
-}
-/**
- *
- *  * rev_string - returns the length of a given string
- *    * @s: string to be reversed
- *     * Return: returns the length of string
+ *rev_string - returns the length of a given string
+ *@s: string to be reversed
+ *Return: returns the length of string
  **/
 void rev_string(char *s)
 {
 	int i;
+
 	int len;
-	char a;
-	char z;
-	len = _strlen(s) - 1;
-	i = 0; 
-	while (i < len)
+
+	len = strlen(s);
+	for (i = len - 1; i >= 0; i--)
 	{
-		a = s[i];
-		z = s[len];
-		s[i++] = z;
-		s[len--] = a;
-	}	
+	_putchar(len);
+	}
+}
