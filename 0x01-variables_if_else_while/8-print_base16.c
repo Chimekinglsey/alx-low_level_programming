1e3abc022284e1ddee39c1a96ef2ad4a46b144c3
chimekinglsey <onlinekingsley@gmail.com>
2022-10-27 05:43:17 -0700

    testing

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Produces numbers in base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int A;
+
+char B;
+
+for (A = 0; A <= 9; A++)
+{
+	putchar('0' + A);
+
+	for (B = 'a'; B <= 'f'; B++)
+	putchar(B);
+}
+
+	putchar('\n');
+return (0);
+}
