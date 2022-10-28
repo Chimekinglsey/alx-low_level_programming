2cbdb3da601e72b18076eafe2d332370c020bb10
chimekinglsey <onlinekingsley@gmail.com>
2022-10-28 05:50:48 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  * add - main prints lowercase
+ *
+ *  @a: int to add
+ *
+ *  @b: int to add second
+ *
+ *   * Return: A pointer to the resulting string
+ *
+ **/
+int add(int a, int b)
+{
+		return (a + b);
+}
