97cf238e03abba18bd80ab426e25adbc00964a58
chimekinglsey <onlinekingsley@gmail.com>
2022-10-27 19:49:28 -0700

    more krctns

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,17 +7,21 @@
  **/
 void print_alphabet_x10(void)
 {
-char A = 'a';
 
-while (A <= 'z')
-{
-	int i;
+int i;
 
-	for (i = 0; i <= 10; i++)
+for (i = 0; i <= 10; i++)
 	{
+	char A = 'a';
+
+	while (A <= 'z')
+	{
+
 	_putchar(A);
-	}
+
 	A++;
-}
+	}
+
 _putchar('\n');
+}
 }
