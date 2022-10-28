5764cfd27c60a5687e19955f5acac36ce554acb3
chimekinglsey <onlinekingsley@gmail.com>
2022-10-28 14:22:04 -0700

    trendz

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -17,21 +17,17 @@ double largest_number(double a, double b, double c)
 {
 	double largest;
 
-	if ( a == b == c)
+	if ((a >= b) && (a >= c))
 	{
 		largest = a;
 	}
-
-	else if ((a >= b) && (b >= c))
-	{
-		largest = a;
-	}
-	else if ((b >= a) && (a >= c))
+	else if ((b >= a) && (b >= c))
 	{
 		largest = b;
 	}
-	else
+	else if ((c >= a) && (c >= b))
 	{
+
 		largest = c;
 	}
 	return (largest);
