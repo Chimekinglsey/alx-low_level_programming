36f290f985ebbfc30ddf05d693419a70703958a2
chimekinglsey <onlinekingsley@gmail.com>
2022-10-28 04:00:00 -0700

    6 abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *  * _abs - main prints lowercase
+ *
+ *  @r: the parameter
+ *
+ *   * Return: A pointer to the resulting string
+ *
+ **/
+int _abs(int r)
+
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+
+	else
+	{
+		return (r * -1);
+	}
+}
