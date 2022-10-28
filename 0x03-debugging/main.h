5f0d4059dbb20069a9ef8c4977da1719d20b6c88
chimekinglsey <onlinekingsley@gmail.com>
2022-10-28 13:53:47 -0700

    Date test2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
-int convert_day(int, int);
-void print_remaining_days(int, int, int);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 double largest_number(double, double, double);
 void positive_or_negative(int i);
 #endif
