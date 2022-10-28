cb579b2ba0b276c0adb26963ee2f65a35b71913a
chimekinglsey <onlinekingsley@gmail.com>
2022-10-28 07:07:05 -0700

    a

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  * main - main prints lowercase
+ *
+ *   * Return: A pointer to the resulting string
+ *
+ **/
+int main(void)
+
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+return (0);
+
+}
