df9f8b3766bc06119d930f3bc9b2311336b3791e
chimekinglsey <onlinekingsley@gmail.com>
2022-10-27 18:43:42 -0700

    burning out

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,12 +6,12 @@
  **/
 int main(void)
 {
-char put[7] = "putchar";
+char put[8] = "_putchar";
 
 int i;
-for (i = 0; i < 8; i++)
+
+for (i = 0; i < 9; i++)
 {
-	_putchar('_');
 	_putchar(put[i]);
 }
 _putchar('\n');
