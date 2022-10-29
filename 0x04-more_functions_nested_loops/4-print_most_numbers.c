66abe67e6efa0d4b12b113d9b6943dcf737d5973
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 16:09:44 -0700

    always check with betty and full gcc flags before wasting yout push data

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,7 +9,7 @@ void print_most_numbers(void)
 char upper;
 	for (upper = '0'; upper <= '9'; upper++)
 	{
-		if (upper !== 2 && upper != 4)
+		if (upper != 2 && upper != 4)
 		{
 		_putchar(upper);
 		}
