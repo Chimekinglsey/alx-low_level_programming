b5766d4d282456a2847a6cdbed196aa7fc0121a2
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 16:27:15 -0700

    new more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+ *  * more_numbers - prints 0-14 in 10 different line;
+ *   * Return: nothing
+ *
+ **/
+void more_numbers(void)
+{
+char num;
+int counter = 0;
+while (counter <= 10)
+{
+for (num = '0'; num <= '14'; num++)
+	{
+	_putchar(num)
+	}
+}
+_putchar('\n');
+counter++;
+}
+}
