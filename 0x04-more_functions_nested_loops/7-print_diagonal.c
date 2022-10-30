8e1e728c9f1751c220b3bd9b2cf93bd0cac8c3a2
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 17:48:26 -0700

    diagonal sleep: free world best

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *  * print_diagonal - prints a given number of line;
+ *  @n: number of lines to print
+ *   * Return: nothing
+ *
+ **/
+void print_diagonal(int n)
+{
+int i;
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('\\');
+	}
+	_putchar('\n');
+}
