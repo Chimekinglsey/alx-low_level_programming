2f291ee9b80a3dcab5d3b695f04c9d6f77645f37
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 17:38:55 -0700

    print line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  * print_line - prints a given number of line;
+ *  @n: number of lines to print
+ *   * Return: nothing
+ *
+ **/
+void print_line(int n)
+{
+int i;
+	for (i = 0; i < n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		_putchar('_');
+	}
+}
