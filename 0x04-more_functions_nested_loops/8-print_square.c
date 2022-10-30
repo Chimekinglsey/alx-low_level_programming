8e7b82ae814df694844c32f4fddf39625cc232d6
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 19:40:04 -0700

    Thank You Lord

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,25 +12,19 @@ void print_square(int size)
 int i;
 int j;
 
-if (size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 	}
-else 
-{
-for (j = 0; j < size; j++)
+	else
 	{
-	for (i = 0; i < size; i++)
-	{
-		/**
-		if (size <= 0)
+	for (j = 0; j < size; j++)
 		{
-			_putchar('\n');
+		for (i = 0; i < size; i++)
+		{
+			_putchar('#');
 		}
-		*/
-		_putchar('#');
+		_putchar('\n');
+		}
 	}
-	_putchar('\n');
-}
-}
 }
