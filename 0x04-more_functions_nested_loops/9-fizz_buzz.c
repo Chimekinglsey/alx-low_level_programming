ab84634ee0410c6b4afe5333bfc98b9bc80d09e7
chimekinglsey <onlinekingsley@gmail.com>
2022-10-29 20:51:59 -0700

    Happy Sunday

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,17 @@ int main(void)
 int i;
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 15 == 0)
+		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("Fizzbuzz ");
 		}
-			else if (i % 5 == 0)
+			else if (i % 3 == 0)
 			{
-				printf("Buzz ");
+				printf("Fizz ");
 			}
-				else if (i % 3 == 0)
+				else if (i % 5 == 0)
 				{
-					printf("Fizz ");
+					printf("Buzz ");
 				}
 				else
 				{
