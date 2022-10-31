295cf48e5d51d2af8b289deebe1929ee12e21a95
chimekinglsey <onlinekingsley@gmail.com>
2022-10-31 15:36:19 -0700

    testing memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  *string_toupper - a function that converts lowecase to Uppers
+ *
+ * @s: Pointer to string
+ *
+ *   * Return: A pointer to the resulting string
+ *
+ **/
+char *_memset(char *s, char b, unsigned int n)
+{
+_memset(s, b, n);
+return (s);
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <string.h>
+int _putchar(char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /*end of header file*/
