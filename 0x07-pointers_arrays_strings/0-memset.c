b06ce6115f7b46130d44f8e9be59d75c79b0b8d9
chimekinglsey <onlinekingsley@gmail.com>
2022-10-31 16:22:11 -0700

    moree

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,14 +1,19 @@
 #include "main.h"
 /**
- *  *string_toupper - a function that converts lowecase to Uppers
+ *  *_memset - a function that sets a value to s
  *
  * @s: Pointer to string
- *
+ * @b: the new value
+ * @n: the number of bytes to set
  *   * Return: A pointer to the resulting string
  *
  **/
 char *_memset(char *s, char b, unsigned int n)
 {
-_memset(s, b, n);
+	/**
+	*char *new_s;
+	*new_s = s;
+	*/
+	_memset(s, b, n * sizeof(char));
 return (s);
 }
