9caaf87d42f57a1496a07a59446a7d48ab6ae149
chimekinglsey <onlinekingsley@gmail.com>
2022-10-31 19:05:36 -0700

    cHECKERS

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *  *_strchr - a function that sets a value to s
+ *
+ * @s: Pointer to string
+ * @c: the new value to search
+ *   * Return: A pointer to the resulting string
+ *
+ **/
+char *_strchr(char *s, char c)
+{
+	char caps[27] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char small[27] = {"abcdefghijklmnopqrstuvwxyz"};
+	int i;
+
+	for (i = 0; i < 27; i++)
+	{
+		if ((c != caps[i]) || (c != small[i]))
+		{
+			s = NULL;
+		}
+		else if (c == caps[i])
+		{
+			s = &caps[i];
+		}
+		else if (c == small[i])
+		{
+			s = &small[i];
+		}
+	
+	}
+return (s);
+}
