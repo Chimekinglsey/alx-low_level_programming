b0b6864203012e8f20857320dc113db627596301
chimekinglsey <onlinekingsley@gmail.com>
2022-11-01 06:18:46 -0700

    Learning to Think in loops

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ *  *_strspn - a function that sets a value to s
+ *
+ * @s: Pointer to destination string
+ *
+ * @accept: the character string to check
+ *   * Return: String length
+ *
+ **/
+unsigned int _strspn(char *s, char *accept)
+{
+int string_length = 0;
+int strlen_s = strlen(s);
+int i;
+int j;
+
+	for (i = 0; i < strlen_s; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+		if (accept[j] == s[i])
+		{
+			string_length++;
+			break;
+		}
+		else
+			continue;
+		}
+	}
+return (string_length);
+}
