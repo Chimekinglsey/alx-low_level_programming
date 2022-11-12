20c7ea9a1f2abd1ebf51599f8dfe94c4cf82c6a6
chimekinglsey <onlinekingsley@gmail.com>
2022-11-12 10:28:40 -0800

    First commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  *_puts_recursion - a function that prints the  value of s
+ *
+ * @s: Pointer to string
+ *
+ *   * Return: nothing
+ *
+ **/
+void _puts_recursion(char *s)
+{
+_putchar(s);
+_putchar('\n');
+}
+
