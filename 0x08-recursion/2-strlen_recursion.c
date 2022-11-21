1cecf2b507bf8a587ac3ba66a5d05a323eb58225
chimekinglsey <onlinekingsley@gmail.com>
2022-11-21 09:12:31 -0800

    recursion works like loop

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,11 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s)
+	if (*s != '\0')
 	{
 		i++;
-	return (i + _strlen_recursion(s + 1));
+		return (i);
+	return (_strlen_recursion(s + 1));
 	}
 return (0);
 }
