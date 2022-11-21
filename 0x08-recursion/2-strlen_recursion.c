51d6796df970421aaae87d241ee5160be323a498
chimekinglsey <onlinekingsley@gmail.com>
2022-11-21 08:41:45 -0800

    recursion works like look

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  *_strlen_recursion - a function that prints the length of s
+ *
+ * @s: Pointer to string
+ *
+ *   * Return: string length of s
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s != '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+	}
+	return (i);
+}
