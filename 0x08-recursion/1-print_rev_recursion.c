47b5a3e8353675e691d280c3e37156d40ae5464c
chimekinglsey <onlinekingsley@gmail.com>
2022-11-21 07:11:08 -0800

    Second

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * _print_rev_recursion - function to print reversed string
+ *
+ * @s: input string
+ * Return - nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s - 1);
+			_putchar(*s);
+	}
+	else
+		_putchar('\0');
+}
