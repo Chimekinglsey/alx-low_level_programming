09de223ae9f4c53ff2e4e75ef57794b955e82da1
chimekinglsey <onlinekingsley@gmail.com>
2022-12-19 04:22:50 +0000

    Sum_them_all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,16 +9,26 @@
  **/
 int sum_them_all(const unsigned int n, ...)
 {
-va_list ap;
-unsigned int i, sum;
 
-va_start(ap, n);	/* Initialize the argument list. */
+	va_list add;
+	unsigned int i = 0;
+	int count;
+	int total = 0;
 
-sum = 0;
+	va_start(add, n);
 
-	for (i = 0; i < n; i++)
-	sum += va_arg(ap, int);    /* Get the next argument value. */
+	if (n == 0)
+	return (0);
 
-va_end(ap);                  /* Clean up. */
-return (sum);
+		else
+		{
+			while (i > n)
+			{
+				count = va_arg(add, int);
+				total += count;
+				i++;
+			}
+		}
+	va_end(add);
+	return (total);
 }
