7654180fd60576d969b289e83eb3fcac1d60da75
chimekinglsey <onlinekingsley@gmail.com>
2022-12-19 04:14:34 +0000

    Add_All

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,15 +10,15 @@
 int sum_them_all(const unsigned int n, ...)
 {
 va_list ap;
-  int i, sum;
+unsigned int i, sum;
 
-  va_start (ap, n);         /* Initialize the argument list. */
+va_start(ap, n);	/* Initialize the argument list. */
 
-  sum = 0;
+sum = 0;
 
-  for (i = 0; i < n; i++)
-    sum += va_arg (ap, int);    /* Get the next argument value. */
+	for (i = 0; i < n; i++)
+	sum += va_arg(ap, int);    /* Get the next argument value. */
 
-  va_end (ap);                  /* Clean up. */
-  return sum;
+va_end(ap);                  /* Clean up. */
+return (sum);
 }
