92544ac83758042b3747b8dc9f0c79dc93a3b64f
chimekinglsey <onlinekingsley@gmail.com>
2022-12-25 23:34:00 +0000

    Modified

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,28 +10,23 @@
 * Return: always zero at success
 *
 */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-int count, result = 0;
+	int num, result = 0, i;
 
-if (argc == 1)
-	printf("0\n");
-
-		if (argc > 2)
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
 		{
-			for (count = 2; count == argc; count++)
+			if (!(isdigit(argv[argc][i])))
 			{
-				if (isdigit(atoi(argv[count])))
-					{
-					result += (atoi(argv[count]));
-					return (result);
-					}
-				else
-					{
-					printf("Error\n");
-					return (1);
-					}
+				printf("Error\n");
+				return (1);
 			}
 		}
-return (0);
-}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+	return (0);
+	}
