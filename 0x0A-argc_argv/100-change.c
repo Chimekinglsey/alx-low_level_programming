d2754c62d7ef1852095a063fc1eda026e9619f7f
chimekinglsey <onlinekingsley@gmail.com>
2022-12-26 00:41:32 +0000

    modified

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,50 +15,42 @@ int main(int argc, char *argv[])
 int amount, change = 0;
 amount = atoi(argv[1]);
 
-	if (argc < 2)
-		{
-		printf("Error\n");
-		return (1);
-		}
-	if (argc != 2)
-		{
-		printf("Error\n");
-		return (1);
-		}
-		if (amount < 0)
-		printf("%d\n", 0);
+if (argc != 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
+if (amount < 0)
+	printf("%d\n", 0);
 
+	else
+	{
 		if (amount % 25 >= 0)
 		{
 			change += amount / 25;
 			amount = amount % 25;
-			printf("%d\n", change);
 		}
 		if (amount % 10 >= 0)
 		{
 			change += amount / 10;
 			amount = amount % 10;
-			printf("%d\n", change);
 		}
 		if (amount % 5 >= 0)
 		{
 			change += amount / 5;
 			amount = amount % 5;
-			printf("%d\n", change);
 		}
 		if (amount % 2 >= 0)
 		{
 			change += amount / 2;
 			amount = amount % 2;
-			printf("%d\n", change);
 		}
 		if (amount % 1 >= 0)
 		{
 			change += amount / 1;
 			amount = amount % 1;
-			printf("%d\n", change);
 		}
-
+	printf("%d\n", change);
+	}
 return (0);
 }
-
