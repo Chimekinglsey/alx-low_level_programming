b1142fdf561ea0e1e1be14647924ded386d01aba
chimekinglsey <onlinekingsley@gmail.com>
2022-12-26 00:43:11 +0000

    modified

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -15,6 +15,12 @@ int main(int argc, char *argv[])
 int amount, change = 0;
 amount = atoi(argv[1]);
 
+
+if (argc < 2)
+	{
+	printf("Error\n");
+	return (1);
+	}
 if (argc != 2)
 	{
 	printf("Error\n");
