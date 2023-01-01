9e4a6d7bfd742fb34ffa63ef0421bb566b3a96db
chimekinglsey <onlinekingsley@gmail.com>
2023-01-01 13:21:13 +0000

    concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,41 +9,28 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	char *newv, *newv2, *dups1, *dups2, *sum;
+	char *sum;
 	int i, len1, len2, len3;
 
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	len3 = len1 + len2;
+	len3 = len1 + len2 + 1;
 
-	dups1 = s1;
-	dups2 = s2;
-
-
-	newv = (char *) malloc((len1) * sizeof(char));
-	newv2 = (char *) malloc((len2) * sizeof(char));
-
-
-	for (i = 0; i < len1; i++)
-	{
-		dups1[i] = newv[i];
-	}
-	for (i = 0; i < len2; i++)
-	{
-		dups2[i] = newv2[i];
-	}
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	sum = (char *) malloc(len3 * sizeof(char));
 	for (i = 0; i < len1; i++)
 	{
-	sum[i] = s1[i];
+	sum[i] = *s1;
 	}
 	for (; i >= len1 - 1; i++)
 	{
 		if (i > (len1 - 1) && i <= len2)
 		{
-		sum[i] = s2[i];
+		sum[i] = *s2;
 		}
 	}
 	return (sum);
