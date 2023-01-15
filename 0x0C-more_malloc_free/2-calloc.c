07af212450e280adac9c6c5613e98c6b37681507
chimekinglsey <onlinekingsley@gmail.com>
2023-01-15 23:06:40 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,16 +7,16 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-unsigned int i, *array;
-
+unsigned int i, j, *array;
+j = nmemb * size;
 if (nmemb == 0 || size == 0)
 return (NULL);
 
-array = malloc(nmemb * size);
+array = malloc(j);
 if (array == NULL)
 return (NULL);
 
-for (i = 0; i < nmemb; i++)
+for (i = 0; i < j; i++)
 	array[i] = 0;
 
 return (array);
