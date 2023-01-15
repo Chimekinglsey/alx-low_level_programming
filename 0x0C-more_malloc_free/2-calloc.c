07d8e7fb359e029e9a4b6cda7d1d1072d3f1f1d9
chimekinglsey <onlinekingsley@gmail.com>
2023-01-15 22:51:28 +0000

    calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _calloc - allocates memory
+ * @nmemb: number of array members
+ * @size: size of bytes to allocate
+ * Return: A pointer to concatenated string memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+unsigned int i, *array;
+
+if (nmemb == 0 || size == 0)
+return (NULL);
+
+array = malloc(nmemb * size);
+if (array == NULL)
+return (NULL);
+
+for (i = 0; i < nmemb; i++)
+	array[i] = 0;
+
+return (array);
+}
