25ff350a044ec449f9dccb0b35821c0eb427a5d0
chimekinglsey <onlinekingsley@gmail.com>
2023-01-15 22:28:30 +0000

    malloc more

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -6,36 +6,33 @@
  * @n: an integer extent to concat
  * Return: A pointer to concatenated string memory
  */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *s3;
-unsigned int j, i = 0;
+	char *new_str;
+	unsigned int i = 0, lens1 = strlen(s1), lens2 = strlen(s2);
 
-		if (s1 == NULL)
-			s1  = "";
+	if (s1 == NULL)
+		s1 = "";
 
-		if (s2 == NULL)
-			s2 = "";
 
-		s3 = malloc(strlen(s1) + n + 1);
+	if (s2 == NULL)
+		s2 = "";
 
-		if (s3 == NULL)
-			return (NULL);
 
-		if (n >= (strlen(s2)))
-			n = strlen(s2);
-		
-		while (i < (strlen(s1)))
-			{
-				s3[i] = s1[i];
-				j = i + 1;
-				i++;
-			}
+	if (n >= lens2)
+		n = lens2;
 
-		while (j >= strlen(s1) && j <= strlen(s3))
-			{
-				s3[j] = s2[i];
-				j++;
-			}
-		return (s3);
+	new_str = malloc(lens1 + n + 1);
+	if (new_str == NULL)
+		return (NULL);
+
+	for (; i < (lens1 + n); i++)
+	{
+		if (i < lens1)
+			new_str[i] = s1[i];
+		else
+			new_str[i] = s2[i];
+	}
+	new_str[i] = '\0';
+	return (new_str);
 }
