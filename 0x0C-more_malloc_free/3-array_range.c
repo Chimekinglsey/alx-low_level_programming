5546004e13edc46d65b1063ec9803359ac819831
chimekinglsey <onlinekingsley@gmail.com>
2023-01-16 00:07:16 +0000

    integer array malloc

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * array_range - allocates memory to array
+ * @min: start range
+ * @max: stop range
+ * Return: A pointer to concatenated string memory
+ */
+int *array_range(int min, int max)
+{
+int diff, i,  *ar;
+
+diff = max - min;
+ar = malloc(diff *sizeof(int));
+	if (ar == NULL)
+		return (NULL);
+
+	if (min > max)
+		{
+		return (NULL);
+		}
+	else
+		{
+			for (i = 0; i <= diff; i++)
+			{
+				ar[i] = min + i;
+			}
+		}
+return (ar);
+}
