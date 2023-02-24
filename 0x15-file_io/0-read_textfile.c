7b5f7624592cf728581ce76a3f3a89948078d83b
chimekinglsey <onlinekingsley@gmail.com>
2023-02-24 17:16:44 +0000

    Initial COmmit

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+*read_textfile - a function that reads a text file and writes into a buffer
+* @filename: name of file to read
+* @letters: number of letters to read and print
+* Return - number of letters read
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t op, rd, wr;
+char* buffer = malloc(sizeof(char) * letters);
+if (filename == NULL)
+	return (0);
+
+op = open(filename, O_RDONLY);
+if (op == -1)
+	return (0);
+
+rd = read(op, buffer, letters);
+if (rd == -1)
+	return (0);
+
+wr = write(STDOUT_FILENO, buffer, rd);
+if (wr == -1)
+	return (0);
+free (buffer);
+close(op);
+return (wr);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,3 @@
+We are seeing how to do file input and output operations 
+first we declare a pointer of struct file (or struct FILE typedefed)
+second we assign it to open a file if its fopen or we use malloc to secure a location in the heap and so on and so on and so on
