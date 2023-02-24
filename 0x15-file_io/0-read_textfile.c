3b5966878db3a52ae6f5cf489fc5c0b3e3d3209d
chimekinglsey <onlinekingsley@gmail.com>
2023-02-24 17:29:32 +0000

    initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,17 +1,20 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
-*read_textfile - a function that reads a text file and writes into a buffer
+/**
+* read_textfile - This function reads a text file and writes it.
+*
 * @filename: name of file to read
+*
 * @letters: number of letters to read and print
-* Return - number of letters read
+*
+* Return: number of letters read
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 ssize_t op, rd, wr;
-char* buffer = malloc(sizeof(char) * letters);
+char *buffer = malloc(sizeof(char) * letters);
 if (filename == NULL)
 	return (0);
 
@@ -26,7 +29,7 @@ if (rd == -1)
 wr = write(STDOUT_FILENO, buffer, rd);
 if (wr == -1)
 	return (0);
-free (buffer);
+free(buffer);
 close(op);
 return (wr);
 }
