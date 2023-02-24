1af7828ec321f130f702f32a498e3142ec56c082
chimekinglsey <onlinekingsley@gmail.com>
2023-02-24 20:52:13 +0000

    Second commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,14 +13,14 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int op, wr, null, len = strlen(text_content);
+int op, wr, len = strlen(text_content);
 
 if (filename == NULL)
 	return (-1);
 
 if (text_content == NULL)
 	{	
-		len = null;
+		len = 0;
 	}
 
 op = open(filename, O_CREAT | O_RDWR |O_TRUNC, 0600);
