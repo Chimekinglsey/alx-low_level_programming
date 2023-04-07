fb21f89f9daf0af9e57229181337670e5a213a15
chimekinglsey <onlinekingsley@gmail.com>
2023-04-07 20:48:07 +0000

    FIrst COmmit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,13 +11,19 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *newtable;
+	hash_table_t *newtable; int i;
 
 	newtable = (hash_table_t *)malloc(sizeof(hash_table_t));
 
 	if (newtable == NULL)
 		return (NULL);
 	newtable->size = size;
-	newtable->array = malloc(newtable->size * sizeof(hash_node_t));
+	newtable->array = malloc(size * sizeof(hash_node_t));
+	if (!newtable->array)
+		free(newtable->array);
+
+	for(i = 0; i < size; i++)
+		newtable->array[i] = NULL;
+	
 	return (newtable);
 }
